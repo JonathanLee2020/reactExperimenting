938b3904ec1f929ebae727f7c9356a00e8eb8468
Jonathan Lee <jonathanlee882@gmail.com>
2023-07-03 14:44:38 +1000

    added styling to input and searchbar

--- react-experimenting/src/pages/Home.jsx
+++ react-experimenting/src/pages/Home.jsx
@@ -1,18 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import "../index.css"
 
 function Home () {
     
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="home__container">
             <h1 className="text-3xl font-bold underline">Welcome to Pokedex!</h1>
-            <h2>test</h2>
-            <h4>fds</h4>
-            <div className="input w-1/2 flex-row items-center justify-center">
+            <div className="input__wrapper">
                 <input 
                 type="text" 
-                className="pokemon h-100 px-4 py-2 text-lg border-2 border-black" 
-                style={{width: "100%"}}
-                placeholder="Enter the Pokemon you want to see or it's dex number"/>
+                className="searchBar"
+                placeholder="Enter the Pokemon name or dex number"/>
                 <button>Search</button>
             </div>
             
